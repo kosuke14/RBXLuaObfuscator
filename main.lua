252edef287e503a73011829a0e885bd615dfd357
kosukeReboyX <57896923+kosuke14@users.noreply.github.com>
2022-06-12 16:47:35 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,13 +4,13 @@ local src = require("obfuscator")
 local ui = {js_lua_code = nil, js_lua_ob = nil}
 
 function init()
-  local codeform = document:querySelector("#js_lua_code")
+  local codeform = document:querySelector(".js_lua_code")
   if codeform == nil or codeform == js.null then
-    repeat codeform = document:querySelector("#js_lua_code") until codeform ~= nil and codeform ~= js.null
+    repeat codeform = document:querySelector(".js_lua_code") until codeform ~= nil and codeform ~= js.null
   end
-  local codebtn = document:querySelector("#js_lua_ob")
+  local codebtn = document:querySelector(".js_lua_ob")
   if codebtn == nil or codebtn == js.null then
-    repeat codebtn = document:querySelector("#js_lua_ob") until codebtn ~= nil and codebtn ~= js.null
+    repeat codebtn = document:querySelector(".js_lua_ob") until codebtn ~= nil and codebtn ~= js.null
   end
   codebtn:addEventListener("click", function()
     codeform.value = src(codeform.value)
