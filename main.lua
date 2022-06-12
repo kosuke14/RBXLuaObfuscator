31a94c5ca28bce98dc9667482ca262d0fac7796a
kosukeReboyX <57896923+kosuke14@users.noreply.github.com>
2022-06-12 16:26:42 +0900

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,13 @@
+local window = js.global
+local document = window.document
+local src = require("src/main")
+local ui = {js_lua_code = nil, js_lua_ob = nil}
+
+function init()
+  local codeform = document:querySelector("#js_lua_code")
+  local codebtn = document:querySelector("#js_lua_ob")
+  codebtn:addEventListener("click", function()
+    codeform.value = src(codeform.value)
+    codeform:focus()
+  end)
+end
