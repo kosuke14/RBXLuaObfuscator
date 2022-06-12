a2f0b7213ed41b62f97386e0b5168c89092503bf
kosukeReboyX <57896923+kosuke14@users.noreply.github.com>
2022-06-12 16:05:19 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+document.body.innerHTML =
+    '<div>'
+  +   '<h1>RBXLua Obfuscator</h1>'
+  + '</div>'
+  + '<div class="box-js-lua-code">'
+  +   '<textarea id="js_lua_code" placeholder="Paste and Ctrl + Enter to obfuscate"></textarea>'
+  + '</div>'
+  + '<div>'
+  +   '<button id="js_lua_ob">Obfuscate</button>'
+  + '</div>'
+  ;
+js_lua_code.addEventListener("keydown", function(e) {
+  // If Ctrl + Enter then beautify
+  if(e.ctrlKey && e.keyCode === 13) js_lua_ob.click();
+});
+
+js_lua_ob.addEventListener("click", function() {
+  js_lua_code.value = luaOB(js_lua_code.value);
+  js_lua_code.focus();
+});
+
+js_lua_code.focus();
