14282b359ac25210c46691a3478b715ea7f10ad7
kosukeReboyX <57896923+kosuke14@users.noreply.github.com>
2022-06-12 16:26:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -9,14 +9,15 @@ document.body.innerHTML =
   +   '<button id="js_lua_ob">Obfuscate</button>'
   + '</div>'
   ;
+var js_lua_code = document.querySelector("#js_lua_code"), js_lua_ob = document.querySelector("#js_lua_ob");
 js_lua_code.addEventListener("keydown", function(e) {
-  // If Ctrl + Enter then beautify
+  // If Ctrl + Enter then ob
   if(e.ctrlKey && e.keyCode === 13) js_lua_ob.click();
 });
 
-js_lua_ob.addEventListener("click", function() {
+/* js_lua_ob.addEventListener("click", function() {
   js_lua_code.value = luaOB(js_lua_code.value);
   js_lua_code.focus();
-});
+}); */
 
 js_lua_code.focus();
